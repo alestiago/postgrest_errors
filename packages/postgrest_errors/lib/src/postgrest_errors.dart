9f45f7671a101b059b5a9f7273033de6ff78237f
Alejandro Santiago <dev@alestiago.com>
2023-11-03 17:47:16 +0000

    feat: postgrest_errors initial template

--- packages/postgrest_errors/lib/src/postgrest_errors.dart
+++ packages/postgrest_errors/lib/src/postgrest_errors.dart
@@ -1,7 +1,96 @@
-/// {@template postgrest_errors}
-/// A Very Good Project created by Very Good CLI.
+// ignore_for_file: lines_longer_than_80_chars
+
+import 'package:postgrest/postgrest.dart';
+
+/// A function that builds a [PostgrestException].
+typedef PostgrestExceptionBuilder = PostgrestException Function({
+  required String message,
+  String? code,
+  dynamic details,
+  String? hint,
+});
+
+/// An error thrown by PostgREST.
+enum PostgrestError {
+  /// Could not connect with the database due to an incorrect db-uri or due to an internal error.
+  pgrst001._(
+    code: 'PGRST001',
+    httpStatus: '503',
+    description:
+        '''Could not connect with the database due to an internal error.''',
+  );
+
+  const PostgrestError._({
+    required this.code,
+    required this.httpStatus,
+    required this.description,
+  });
+
+  /// The error code.
+  final String code;
+
+  /// The HTTP status code.
+  final String httpStatus;
+
+  /// The error description.
+  final String description;
+
+  static final _codeToExceptionBuilder = <String, PostgrestExceptionBuilder>{
+    'PGRST001': ({
+      required String message,
+      String? code,
+      String? hint,
+      dynamic details,
+    }) {
+      return Postgrest001Exception._(
+        message: message,
+        hint: hint,
+        details: details,
+      );
+    },
+  };
+
+  /// Returns a [PostgrestException] from a [json] response.
+  ///
+  /// The function signature matches [PostgrestException.fromJson].
+  static PostgrestException fromJsonAsPostgrestException(
+    Map<String, dynamic> json, {
+    String? message,
+    int? code,
+    String? details,
+  }) {
+    final exceptionMessage = (json['message'] ?? message) as String;
+    final exceptionCode = (json['code'] ?? '$code') as String?;
+    final exceptionDetails = (json['details'] ?? details) as dynamic;
+    final exceptionHint = json['hint'] as String?;
+
+    final exceptionBuilder = _codeToExceptionBuilder[exceptionCode];
+    if (exceptionBuilder != null) {
+      return exceptionBuilder(
+        message: exceptionMessage,
+        code: exceptionCode,
+        details: exceptionDetails,
+        hint: exceptionHint,
+      );
+    }
+
+    return PostgrestException(
+      message: exceptionMessage,
+      code: exceptionCode,
+      details: exceptionDetails,
+      hint: exceptionHint,
+    );
+  }
+}
+
+/// {@template Postgrest001Exception}
+/// Could not connect with the database due to an internal error.
 /// {@endtemplate}
-class PostgrestErrors {
-  /// {@macro postgrest_errors}
-  const PostgrestErrors();
+class Postgrest001Exception extends PostgrestException {
+  /// {@macro Postgrest001Exception}
+  Postgrest001Exception._({
+    required super.message,
+    super.hint,
+    super.details,
+  }) : super(code: PostgrestError.pgrst001.code);
 }
