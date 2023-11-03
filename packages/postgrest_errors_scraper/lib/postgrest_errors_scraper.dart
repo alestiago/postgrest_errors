df043a9660314c3c30c33070a316d64a42638877
Alejandro Santiago <dev@alestiago.com>
2023-11-03 16:33:56 +0000

    chore: create postgrest_error_scrapper

---
+++ packages/postgrest_errors_scraper/lib/postgrest_errors_scraper.dart
@@ -0,0 +1,4 @@
+/// A Very Good Project created by Very Good CLI.
+library postgrest_errors_scraper;
+
+export 'src/postgrest_errors_scraper.dart';
---
+++ packages/postgrest_errors_scraper/lib/src/postgrest_errors_scraper.dart
@@ -0,0 +1,7 @@
+/// {@template postgrest_errors_scraper}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class PostgrestErrorsScraper {
+  /// {@macro postgrest_errors_scraper}
+  const PostgrestErrorsScraper();
+}
