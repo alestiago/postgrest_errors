a59d57fab9e4eabc4cedd87df42648f3151e18fe
Alejandro Santiago <dev@alestiago.com>
2023-11-03 16:35:02 +0000

    chore: postgrest_errors

---
+++ packages/postgrest_errors/lib/postgrest_errors.dart
@@ -0,0 +1,4 @@
+/// A Very Good Project created by Very Good CLI.
+library postgrest_errors;
+
+export 'src/postgrest_errors.dart';
---
+++ packages/postgrest_errors/lib/src/postgrest_errors.dart
@@ -0,0 +1,7 @@
+/// {@template postgrest_errors}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class PostgrestErrors {
+  /// {@macro postgrest_errors}
+  const PostgrestErrors();
+}
