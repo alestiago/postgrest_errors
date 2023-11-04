dc44645c01b4aa6b42de0ccb7571b9722eb6bccb
Alejandro Santiago <dev@alestiago.com>
2023-11-04 06:29:08 +0000

    feat: made message in JSON optional (#8)

--- packages/postgrest_errors/test/src/postgrest_errors_test.dart
+++ packages/postgrest_errors/test/src/postgrest_errors_test.dart
@@ -6,340 +6,238 @@ void main() {
     group('fromJson', () {
       test('returns a raw PostgresException when code is not found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': 'not_found',
-            'message': 'message',
-          },
+          {'code': 'not_found'},
         );
         expect(exception, isA<PostgrestException>());
       });
 
       test('returns a Postgrest000Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst000.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst000.code},
         );
         expect(exception, isA<Postgrest000Exception>());
       });
 
       test('returns a Postgrest001Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst001.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst001.code},
         );
         expect(exception, isA<Postgrest001Exception>());
       });
 
       test('returns a Postgrest002Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst002.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst002.code},
         );
         expect(exception, isA<Postgrest002Exception>());
       });
 
       test('returns a Postgrest003Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst003.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst003.code},
         );
         expect(exception, isA<Postgrest003Exception>());
       });
 
       test('returns a Postgrest100Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst100.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst100.code},
         );
         expect(exception, isA<Postgrest100Exception>());
       });
 
       test('returns a Postgrest101Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst101.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst101.code},
         );
         expect(exception, isA<Postgrest101Exception>());
       });
 
       test('returns a Postgrest102Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst102.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst102.code},
         );
         expect(exception, isA<Postgrest102Exception>());
       });
 
       test('returns a Postgrest103Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst103.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst103.code},
         );
         expect(exception, isA<Postgrest103Exception>());
       });
 
       test('returns a Postgrest105Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst105.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst105.code},
         );
         expect(exception, isA<Postgrest105Exception>());
       });
 
       test('returns a Postgrest106Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst106.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst106.code},
         );
         expect(exception, isA<Postgrest106Exception>());
       });
 
       test('returns a Postgrest107Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst107.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst107.code},
         );
         expect(exception, isA<Postgrest107Exception>());
       });
 
       test('returns a Postgrest108Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst108.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst108.code},
         );
         expect(exception, isA<Postgrest108Exception>());
       });
 
       test('returns a Postgrest109Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst109.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst109.code},
         );
         expect(exception, isA<Postgrest109Exception>());
       });
 
       test('returns a Postgrest110Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst110.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst110.code},
         );
         expect(exception, isA<Postgrest110Exception>());
       });
 
       test('returns a Postgrest111Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst111.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst111.code},
         );
         expect(exception, isA<Postgrest111Exception>());
       });
 
       test('returns a Postgrest112Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst112.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst112.code},
         );
         expect(exception, isA<Postgrest112Exception>());
       });
 
       test('returns a Postgrest113Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst113.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst113.code},
         );
         expect(exception, isA<Postgrest113Exception>());
       });
 
       test('returns a Postgrest114Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst114.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst114.code},
         );
         expect(exception, isA<Postgrest114Exception>());
       });
 
       test('returns a Postgrest115Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst115.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst115.code},
         );
         expect(exception, isA<Postgrest115Exception>());
       });
 
       test('returns a Postgrest116Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst116.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst116.code},
         );
         expect(exception, isA<Postgrest116Exception>());
       });
 
       test('returns a Postgrest117Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst117.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst117.code},
         );
         expect(exception, isA<Postgrest117Exception>());
       });
 
       test('returns a Postgrest118Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst118.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst118.code},
         );
         expect(exception, isA<Postgrest118Exception>());
       });
 
       test('returns a Postgrest119Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst119.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst119.code},
         );
         expect(exception, isA<Postgrest119Exception>());
       });
 
       test('returns a Postgrest120Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst120.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst120.code},
         );
         expect(exception, isA<Postgrest120Exception>());
       });
 
       test('returns a Postgrest200Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst200.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst200.code},
         );
         expect(exception, isA<Postgrest200Exception>());
       });
 
       test('returns a Postgrest201Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst201.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst201.code},
         );
         expect(exception, isA<Postgrest201Exception>());
       });
 
       test('returns a Postgrest202Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst202.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst202.code},
         );
         expect(exception, isA<Postgrest202Exception>());
       });
 
       test('returns a Postgrest203Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst203.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst203.code},
         );
         expect(exception, isA<Postgrest203Exception>());
       });
 
       test('returns a Postgrest204Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst204.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst204.code},
         );
         expect(exception, isA<Postgrest204Exception>());
       });
 
       test('returns a Postgrest300Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst300.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst300.code},
         );
         expect(exception, isA<Postgrest300Exception>());
       });
 
       test('returns a Postgrest301Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst301.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst301.code},
         );
         expect(exception, isA<Postgrest301Exception>());
       });
 
       test('returns a Postgrest302Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrst302.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrst302.code},
         );
         expect(exception, isA<Postgrest302Exception>());
       });
 
       test('returns a PostgrestX00Exception when code is found', () {
         final exception = PostgrestError.fromJson(
-          {
-            'code': PostgrestError.pgrstX00.code,
-            'message': 'message',
-          },
+          {'code': PostgrestError.pgrstX00.code},
         );
         expect(exception, isA<PostgrestX00Exception>());
       });
